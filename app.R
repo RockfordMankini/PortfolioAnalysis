f0500772e3db8eab06722f09b4d4ed0d2b9f9391
Rusty Shackleford <haeso55@gmail.com>
2021-05-10 20:36:56 -0700

    Added risk free asset/optimal portfolio solution for historical covariance model. Updated interface (other models not useable).

--- app.R
+++ app.R
@@ -14,39 +14,65 @@ source("stocks.R")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
+    
     # Application title
     titlePanel("Portfolio Analysis Tool"),
-
-        sidebarLayout(
-            sidebarPanel(
-                helpText("Upload a .csv of your monthly returns. The format of the data needs to be such that each column is a stock and each row is
-                         a closing value, with the earliest dates being at the top of the .csv file. Sample data below:"),
-                downloadButton("sample_data", "Sample Data"),
-                br(),
-                br(),
-                fileInput("upload", "Upload a file"),
-                actionButton("minRiskPortfolio", label = "Get Minimum Risk Portfolio"),
-                numericInput("num", label = h3("Get Portfolio For Expected Value"), value = .01, step=.01),
-                actionButton("portfolioFromReturn", label = "Calculate")
-        ),
-
+    
+    sidebarLayout(
+        sidebarPanel(
+            tabsetPanel(type="tabs",
+                        tabPanel("Setup", 
+                                 helpText("Upload a .csv of your monthly returns. The format of the data needs 
+                                    to be such that each column is a stock and each row is
+                                    a closing value, with the earliest dates being at the top of the .csv file. Sample data below:"),
+                                 downloadButton("sample_data", "Sample Data"),
+                                 br(),
+                                 br(),
+                                 fileInput("stock_data", "Upload Stock Data"),
+                                 helpText("If you want to analyze the portfolio using the single index model, upload a market index
+                                              such as the S&P 500 (in the same format as the previous data) below. Sample Index Data below:"),
+                                 downloadButton("sample_data_index", "Sample Data (S&P 500)"),
+                                 br(),
+                                 br(),
+                                 fileInput("index_data", "Upload Index Data (Optional)")),
+                        
+                        tabPanel("Model Selection", 
+                                 selectInput("method", label="Select a model", 
+                                             choices=c("Historical Covariance"="historical"))),
+                        tabPanel("Multi-Group Model",
+                                 helpText("This requires that the uploaded stock data is formatted such that stocks
+                                              in the same industry are in adjacent columns. Breaks also must be input so that the model
+                                              can tell which stocks belong to which industry. For example, if you have 3 industries for 3 stocks
+                                              each, the breaks would be input as '3, 6, 9'."), 
+                                 br(),
+                                 textInput("industry_breaks", "Breaks For Industries", "3, 6, 9")),
+                        
+                        tabPanel("Find Portfolios", 
+                                 actionButton("minRiskPortfolio", label = "Get Minimum Risk Portfolio"),
+                                 numericInput("num", label = h3("Get Portfolio For Expected Value"), value = .01, step=.01),
+                                 actionButton("portfolioFromReturn", label = "Calculate"), 
+                                 br(), 
+                                 numericInput("rf", label = h3("Add Risk Free Asset"), value = 0, step=.001),
+                                 textInput("rfName", "RF Name", "RF"),
+                                 actionButton("optimumPortfolio", label = "Find Optimum Portfolio"),
+                                 actionButton("clearPortfolios", label="Clear Portfolios")))),
+        
         # Show a plot of the generated distribution
         mainPanel(
-           h2("Efficient Frontier"),
-           h6("(File Uploaded Needed To Display)"),
-           plotlyOutput("frontier"),
-           h2("Portfolio"),
-           textInput("portfolioName", "Portfolio Name", "New Portfolio"),
-           actionButton("addPortfolio", label = "Add To Plot"),
-           br(),
-           br(),
-           tabsetPanel(type = "tabs",
-                       tabPanel("Plot", plotlyOutput("portfolio_hist")),
-                       tabPanel("Summary", verbatimTextOutput("summary")),
-                       tabPanel("Table", DT::dataTableOutput("portfolio_table")),
-                       tabPanel("Stock Data", DT::dataTableOutput("stock_data"))
-           ),
+            h2("Efficient Frontier"),
+            h6("(File Uploaded Needed To Display)"),
+            plotlyOutput("frontier"),
+            h2("Portfolio"),
+            textInput("portfolioName", "Portfolio Name", "New Portfolio"),
+            actionButton("addPortfolio", label = "Add To Plot"),
+            br(),
+            br(),
+            tabsetPanel(type = "tabs",
+                        tabPanel("Plot", plotlyOutput("portfolio_hist")),
+                        tabPanel("Summary", verbatimTextOutput("summary")),
+                        tabPanel("Table", DT::dataTableOutput("portfolio_table")),
+                        tabPanel("Stock Data", DT::dataTableOutput("stock_data"))
+            ),
         )
     )
 )
@@ -54,7 +80,7 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+    
     output$sample_data <- downloadHandler(
         filename = function() {
             paste0("sample.csv")
@@ -64,25 +90,52 @@ server <- function(input, output) {
         }
     )
     
+    output$sample_data_index <- downloadHandler(
+        filename = function() {
+            paste0("sample_index.csv")
+        },
+        content = function(file) {
+            write.csv(read.csv("index.csv"), file, fileEncoding = "UTF-8", row.names = FALSE)
+        }
+    )
+    
     data <- reactive({
-        req(input$upload)
-        read.csv(input$upload$datapath)
+        req(input$stock_data)
+        read.csv(input$stock_data$datapath)
     })
     
     frontier <- reactive({
-        req(input$upload)
-        g <- plot_frontier(data())
+        req(input$stock_data)
+        g <- plot_frontier(data(), input$method)
         
         if(length(portfolio_list()) > 0) {
             for(i in 1:length(portfolio_list())) {
                 portfolio <- portfolio_list()[[i]]
                 
-                sd <- get_portfolio_variance(portfolio, data())^.5
+                rf <- portfolio_rf_list()[i]
+                
+                sd <- get_portfolio_variance(portfolio, data(), input$method)^.5
                 expected <- get_portfolio_return(portfolio, data())
                 name <- names(portfolio_list()[i])
-                print(name)
                 
-                g <- g %>% add_trace(x = sd, y = expected, name = name, mode = 'markers')    
+                print(rf)
+                
+                name_rf <- names(rf)
+                
+                g <- g %>% add_trace(x = sd, y = expected, name = name, mode = 'markers') 
+                
+                print(rf)
+                
+                if(rf > 0) {
+                    g <- g %>% add_trace(x = 0, y = rf, name = name_rf, mode='markers')
+                    
+                    slope <- (rf - expected)/(0-sd)
+                    new_x <- sd + .25
+                    new_y <- new_x * slope + rf
+                    
+                    g <- g %>% add_segments(x = new_x, y = new_y, xend = 0, yend = rf, showlegend=FALSE)
+                    
+                }
             }
         }
         
@@ -91,25 +144,57 @@ server <- function(input, output) {
     
     portfolio <- reactiveVal()
     
+    portfolio_rf <- reactiveVal()
+    
     portfolio_list <- reactiveVal(list())
     
+    portfolio_rf_list <- reactiveVal(numeric())
+    
     observeEvent(input$portfolioFromReturn, {
-        req(portfolio())
-        portfolio(portfolio_from_return(data(), input$num))
+        req(data())
+        portfolio(portfolio_from_return(data(), input$num, input$method))
+        portfolio_rf(0)
     })
     
     observeEvent(input$minRiskPortfolio, {
         req(data())
-        portfolio(get_min_risk(data()))
+        portfolio(get_min_risk(data(), input$method))
+        portfolio_rf(0)
+    })
+    
+    observeEvent(input$optimumPortfolio, {
+        req(data())
+        
+        print(breaks)
+        
+        rf <- input$rf
+        
+        if(rf > 0) {
+            portfolio(get_optimum_portfolio(data(), rf, input$method))
+            portfolio_rf(rf)
+        }
+        
     })
     
     observeEvent(input$addPortfolio, {
         req(portfolio())
+        
         portfolios <- length(portfolio_list())
         new_portfolio_list <- portfolio_list()
         new_portfolio_list[[portfolios + 1]] <- portfolio()
         names(new_portfolio_list)[portfolios + 1] <- input$portfolioName
         portfolio_list(new_portfolio_list)
+        
+        new_rfs <- portfolio_rf_list()
+        new_rfs <- c(new_rfs, portfolio_rf())
+        names(new_rfs)[length(new_rfs)] <- input$rfName
+        portfolio_rf_list(new_rfs)
+        
+    })
+    
+    observeEvent(input$clearPortfolios, {
+        portfolio_list(list())
+        portfolio_rf_list(numeric())
     })
     
     output$frontier <- renderPlotly({
@@ -117,7 +202,8 @@ server <- function(input, output) {
     })
     
     output$summary <- renderText({
-        sd <- get_portfolio_variance(portfolio(), data())^.5
+        req(portfolio())
+        sd <- get_portfolio_variance(portfolio(), data(), input$method)^.5
         expected <- get_portfolio_return(portfolio(), data())
         
         paste("Expected Return: ", expected, "\n", "Risk (Standard Deviation): ", sd, sep="")
--- stocks.R
+++ stocks.R
@@ -12,15 +12,26 @@ get_mean_returns <- function(stocks) {
 }
 
 # get covariance of stocks
-get_covariance_matrix <- function(stocks) {
-  cov(get_returns(stocks))
+get_covariance_matrix <- function(stocks, method) {
+  
+  if(method == "historical") {
+    return(cov(get_returns(stocks)))
+  }
+  
+  # if(method == "mgm") {
+  #   
+  #   corrmat <- cor(get_returns(stocks))
+  #   variances <- diag(cov(get_returns(stocks)))
+  #   
+  # }
+  
 }
 
 # get minimum risk portfolio
-get_min_risk <- function(stocks) {
+get_min_risk <- function(stocks, method) {
   
   means <- get_mean_returns(stocks)
-  covmat <- get_covariance_matrix(stocks)
+  covmat <- get_covariance_matrix(stocks, method)
   
   n <- ncol(stocks)
   
@@ -39,17 +50,17 @@ get_portfolio_return <- function(portfolio, stocks) {
   
 }
 
-get_portfolio_variance <- function(portfolio, stocks) {
+get_portfolio_variance <- function(portfolio, stocks, method) {
   
-  covmat <- get_covariance_matrix(stocks)
+  covmat <- get_covariance_matrix(stocks, method)
   drop(t(portfolio) %*% covmat %*% portfolio)
   
 }
 
-portfolio_from_return <- function(stocks, E) {
+portfolio_from_return <- function(stocks, E, method) {
 
   means <- get_mean_returns(stocks)
-  covmat <- get_covariance_matrix(stocks)
+  covmat <- get_covariance_matrix(stocks, method)
   n <- ncol(stocks)
   
   A <- t(rep(1,n)) %*% solve(covmat) %*% means
@@ -65,13 +76,13 @@ portfolio_from_return <- function(stocks, E) {
   
 }
 
-plot_frontier <- function(stocks) {
+plot_frontier <- function(stocks, method) {
   
   E <- seq(-5, 5, .1)
   
   points_df <- data.frame(expected_return = numeric(), standard_deviations = numeric())
   means <- get_mean_returns(stocks)
-  covmat <- get_covariance_matrix(stocks)
+  covmat <- get_covariance_matrix(stocks, method)
   n <- ncol(stocks)
   
   A <- t(rep(1,n)) %*% solve(covmat) %*% means
@@ -120,27 +131,162 @@ plot_portfolio <- function(portfolio) {
   
 }
 
-#plot_portfolio((portfolio_from_return(data, .01)))
-# 
-# data.frame(rownames(f), f[,1])
-# 
-# f <- portfolio_from_return(data, .01)
-# rownames(f)
-# 
-# data <- read.csv("stonks.csv")
-# data <- data[,-(1:3)]
-# write.csv(data, "stonks_cut.csv" ,row.names = FALSE)
-# 
-# View(data.frame(portfolio_from_return(data, .01)))
-# 
-# 
-# get_returns(data)
-# get_mean_returns(data)
-# get_covariance_matrix(data[,1:5])
-# 
-# min_risk <- get_min_risk(data)
-# 
-# get_portfolio_return(min_risk, data)
-# get_portfolio_variance(min_risk, data)
-# 
-# plot_frontier(data)
+### MULTI GROUP MODEL
+multi_group_rho <- function(corrmat, breaks) {
+  
+  industries <- length(breaks)
+  
+  start_inds <- c(1, breaks[1:industries-1] + 1)
+  end_inds <- breaks
+  
+  rho_bar <- matrix(nrow = industries, ncol = industries)
+  
+  for(row in 1:industries) {
+    
+    rows <- start_inds[row]:end_inds[row]
+    
+    for(column in 1:industries) {
+      
+      if(row > column) {
+        next
+      }
+      
+      columns <- start_inds[column]:end_inds[column]
+      
+      rho <- corrmat[rows, columns]
+      
+      if(row == column) {
+        rho <- rho[upper.tri(rho)]
+      }
+      
+      rho_bar[row, column] <- mean(rho)
+      rho_bar[column, row] <- mean(rho)
+      
+    }
+    
+  }
+  
+  rho_bar
+  
+}
+
+make_phi_matrix <- function(rho_bar, breaks, means, variances, rf) {
+  
+  industries <- length(breaks)
+  
+  A <- matrix(nrow = industries, ncol = industries)
+  
+  for(row in 1:industries) {
+    
+    if(row == 1) {
+      N <- breaks[row]
+    }
+    
+    else {
+      N <- breaks[row] - breaks[row - 1]
+    }
+    
+    rho_within <- rho_bar[row, row]
+    
+    for(column in 1:industries) {
+      
+      rho_between <- rho_bar[row, column]
+      
+      if(row == column) {
+        A[row, column] <- 1 + ((N * rho_within) / (1 - rho_within))
+      }
+      
+      else {
+        A[row, column] <- (N * rho_between) / (1 - rho_within)
+      }
+      
+    }
+    
+  }
+  
+  C <- numeric(industries)
+  
+  for(industry in 1:industries) {
+    
+    if(industry == 1) {
+      start_ind <- 1
+    }
+    
+    else {
+      start_ind <- breaks[industry - 1] + 1
+    }
+    
+    end_ind <- breaks[industry]
+    
+    rho <- rho_bar[industry, industry]
+    
+    C[industry] <- sum((means[start_ind:end_ind] - rf) / (variances[start_ind:end_ind]^.5 * (1 - rho)))
+    
+  }
+  
+  solve(A) %*% C
+  
+}
+
+make_multigroup_portfolio <- function(rho_bar, phi, breaks, means, variances, rf, short_sales_allowed = TRUE) {
+  
+  z <- numeric(length(means))
+  z_index <- 1
+  
+  sds <- variances^.5
+  
+  for(industry in 1:length(breaks)) {
+    
+    # get C_star_i
+    c_star <- rho_bar[industry,] %*% phi
+    
+    if(industry == 1) {
+      start_ind <- 1
+    }
+    
+    else {
+      start_ind <- breaks[industry - 1] + 1
+    }
+    
+    end_ind <- breaks[industry]
+    
+    for(stock in start_ind:end_ind) {
+      
+      component_1 <- (1 / (sds[z_index] * (1 - rho_bar[industry, industry])))
+      
+      component_1 <- drop(component_1)
+      
+      component_2 <- (((means[z_index] - rf)/sds[z_index]) - c_star)
+      
+      z[z_index] <- component_1 * component_2
+      z_index <- z_index + 1
+      
+    }
+    
+  }
+  
+  return(z)
+  
+}
+
+### END MULTIGROUP MODEL
+
+get_optimum_portfolio <- function(stocks, rf, method) {
+  
+  if(method == "historical") {
+    
+    returns <- get_mean_returns(stocks)
+    sigma <- get_covariance_matrix(stocks, "historical")
+    
+    z <- solve(sigma) %*% (returns - rf)
+    return(z/sum(z))
+  }
+  
+  if(method == "mgm") {
+    
+    returns <- get_mean_returns(stocks)
+    sigma <- get_covariance_matrix(stocks, "mgm")
+    
+  }
+  
+}
